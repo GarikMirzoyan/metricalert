137df358d32d58d2bf71bdcd4cab0910bbef37c7
Мирзоян Гарик Арегович <mirzoangarikaregovic@MacBook-Pro-Mirzoan.local>
2025-05-15 14:15:30 +0300

    iter13: add body logging

--- internal/middleware/loggermiddleware/loggermiddleware.go
+++ internal/middleware/loggermiddleware/loggermiddleware.go
@@ -1,6 +1,9 @@
 package loggermiddleware
 
 import (
+	"bytes"
+	"compress/gzip"
+	"io"
 	"net/http"
 	"time"
 
@@ -29,16 +32,38 @@ func Logger(next http.Handler, logger *zap.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		// Подготовим кастомный ResponseWriter для получения информации о статусе и размере ответа
+		var bodyBytes []byte
+		if r.Body != nil {
+			var err error
+			if r.Header.Get("Content-Encoding") == "gzip" {
+				gzipReader, err := gzip.NewReader(r.Body)
+				if err != nil {
+					logger.Warn("could not create gzip reader", zap.Error(err))
+				} else {
+					bodyBytes, err = io.ReadAll(gzipReader)
+					_ = gzipReader.Close()
+				}
+			} else {
+				bodyBytes, err = io.ReadAll(r.Body)
+			}
+			if err != nil {
+				logger.Warn("could not read request body", zap.Error(err))
+			}
+		}
+
+		// Восстановить тело запроса
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		// Обернуть writer
 		ww := &statusWriter{ResponseWriter: w}
 		next.ServeHTTP(ww, r)
 
 		duration := time.Since(start)
 
-		// Логируем информацию о запросе и ответе
 		logger.Info("Handled request",
 			zap.String("method", r.Method),
 			zap.String("uri", r.RequestURI),
+			zap.String("body", string(bodyBytes)),
 			zap.Duration("duration", duration),
 			zap.Int("status", ww.status),
 			zap.Int64("response_size", ww.size),
